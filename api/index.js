a23303d562caef2c947dbb6e2c0650e0b15d45ab
gobanoff <a.kliosovas@yahoo.com>
2024-08-05 14:14:50 +0300

    a

--- api/index.js
+++ api/index.js
@@ -3,14 +3,14 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const { connectToDb, PORT } = require("../db");
 const Business = require("../businesses");
-
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
 
 // Middleware to parse JSON request body
 app.use(bodyParser.json());
-
+app.use(cors());
 // Sample data for the shop and items
 let shops = [
   { id: 1, name: "My Shop", location: "123 Main St" },
@@ -36,16 +36,13 @@ app.get("/", (req, res) => {
   res.send("Hello from the Express server!");
 });
 
-const startServer = async () => {
-  try {
-    await connectToDb();
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error("Failed to connect to the database", err);
-    process.exit(1); // Exit the process with a failure code
-  }
-};
+connectToDb()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error('Failed to connect to the database', err);
+  });
 
-module.exports = { app, startServer };
+
+module.exports = app;
